f3f53079e0e62b47f43b79328e0c8c5c0040fc67
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-12-29 15:33:35 +0530

    addnew

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import Classlifecycle from './Lifecycle/Classlifecycle.js';
+
+
+function App() {
+  return (
+    <div>
+     <Classlifecycle/>
+    </div>
+  );
+}
+
+export default App;
